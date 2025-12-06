888cdbfbdd66b51710477abc9ba3bc4c9421e79f
hyo <colleterhyosua@gmail.com>
2025-12-06 11:01:29 +0100

    style: add pop style to theme switcher

--- client/src/components/ui/toggle-mode.tsx
+++ client/src/components/ui/toggle-mode.tsx
@@ -13,28 +13,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      // Le style magique
+      className="rounded-full w-10 h-10 lg:border-2 lg:border-foreground bg-card hover:bg-accent hover:text-accent-foreground shadow-[2px_2px_0px_0px_oklch(var(--foreground))] active:translate-y-[2px] active:translate-x-[2px] active:shadow-none transition-all"
+    >
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all " />
+      ) : (
+        <Moon className=" h-[1.2rem] w-[1.2rem] scale-100 transition-all dark:rotate-0 dark:scale-100" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 }
