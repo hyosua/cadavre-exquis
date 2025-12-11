073f76b3167d9979dc929e5f7f8b00a7e4dde9d3
hyo <colleterhyosua@gmail.com>
2025-12-11 12:08:42 +0100

    fix: Set NODE_ENV for production in start script and configure dotenv path based on environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env'
+});
 
 import express from 'express';
 import { createServer } from 'http';
